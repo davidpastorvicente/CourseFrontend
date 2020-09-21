227c5124ca777ef201d2603c596e9f58927607cf
David <davidpastorvicente@gmail.com>
2020-09-21 16:29:59 +0200

    Change app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -35,7 +34,6 @@ import { MatSortModule } from '@angular/material/sort';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    HttpClientTestingModule,
     BrowserAnimationsModule,
     NgbModule,
     MatToolbarModule,
